4d43a116de89b948fc35a098e4a52888b4dcfea9
Arushad Ahmed <dash-8x@hotmail.com>
2025-03-02 12:19:56 +0500

    - Testing laravel 12 support

--- tests/specs/LangJsCommandTest.php
+++ tests/specs/LangJsCommandTest.php
@@ -50,14 +50,14 @@ class LangJsCommandTest extends TestCase
     /**
      * LangJsCommandTest constructor.
      */
-    public function __construct()
+    public function setUp(): void
     {
-        parent::__construct();
-
         $this->testPath       = __DIR__ . '/..';
         $this->rootPath       = __DIR__ . '/../..';
         $this->outputFilePath = "$this->testPath/output/lang.js";
         $this->langPath       = "$this->testPath/fixtures/lang";
+
+        parent::setUp();
     }
 
     public function _assertStringContainsString($needle, $haystack)
@@ -240,7 +240,7 @@ class LangJsCommandTest extends TestCase
         $code = $this->runCommand($command, ['target' => $this->outputFilePath]);
         $this->assertRunsWithSuccess($code);
         $this->assertFileExists($this->outputFilePath);
-        $this->assertFileNotExists($customOutputFilePath);
+        $this->assertFileDoesNotExist($customOutputFilePath);
 
         $template = "$this->rootPath/src/Mariuzzo/LaravelJsLocalization/Generators/Templates/langjs_with_messages.js";
         $this->assertFileExists($template);
